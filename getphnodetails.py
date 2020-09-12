77afaa165b65d2537b2ada587e40f6d15d0dbcb1
Jayateerth <jayateerth.dambal@gmail.com>
2020-09-12 12:44:05 +0530

    Changed the phone number to user input phone number so that user can input the phone number of his choice with the contry code.

--- getphnodetails.py
+++ getphnodetails.py
@@ -5,7 +5,8 @@ from phonenumbers import geocoder
 from phonenumbers import phonemetadata
 from phonenumbers import carrier
 ##### Change the Country Code according to your own country or Choice of urs
-phone_number = phonenumbers.parse("+91 ")
+ph_num = input("Enter the Phone Number with Country Code:")
+phone_number = phonenumbers.parse(ph_num)
 
 print(geocoder.description_for_number(phone_number,'en'))
 print(carrier.name_for_number(phone_number, 'en'))
